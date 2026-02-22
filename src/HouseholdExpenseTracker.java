f1f2a687c6a31241edb054897e7c57a139d4c8be
Eric-DS-Zhou <zhou.4898@osu.edu>
2026-02-21 21:05:38 -0500

    Finish #2

--- src/HouseholdExpenseTracker.java
+++ src/HouseholdExpenseTracker.java
@@ -40,7 +40,8 @@ public class HouseholdExpenseTracker {
      * @param note
      */
     public void add(String date, String category, double amount, String note) {
-        this.expenses.add(new Expense(date, category, amount, note));
+        String normalizedCategory = category.toLowerCase();
+        this.expenses.add(new Expense(date, normalizedCategory, amount, note));
     }
 
     /**
@@ -76,8 +77,10 @@ public class HouseholdExpenseTracker {
      */
     public double totalForCategory(String category) {
         double sum = 0.0;
+        String targetCategory = category.toLowerCase();
+
         for (Expense e : this.expenses) {
-            if (e.category.equals(category)) {
+            if (e.category.equals(targetCategory)) {
                 sum = sum + e.amount;
             }
         }
@@ -85,8 +88,56 @@ public class HouseholdExpenseTracker {
 
     }
 
+    /**
+     * return a list of expenses in the specifed month
+     * @param yyyyMm
+     * @return
+     */
+    public List<String> expensesInMonth(String yyyyMm) {
+        List<String> result = new ArrayList<>();
+        for (Expense e : this.expenses) {
+            if(e.date.substring(0,6).equals(yyyyMm)){
+                result.add(e.toString());
+            }
+        }
+        return result;
+    }
 
+    /**
+     * print all the expenses
+     */
+    public void printAll() {
+        System.out.println("-----Expenses (" + this.expenses.size() + ")-----");
+        for(int i = 0; i < this.expenses.size(); i++){
+            System.out.println("[" + i + "]" + this.expenses.get(i));
+        }
+        System.out.println("---------------------");
+    }
 
+    //main method (client view)
+    public static void main(String[] args) {
+        HouseholdExpenseTracker tracker = new HouseholdExpenseTracker();
+
+        tracker.add("20260208", "Food", 12.50, "Chipotle");
+        tracker.add("20260210", "Transport", 2.50, "Bus");
+        tracker.add("20260218", "Food", 5.00, "Breakfast");
+        tracker.add("20260228", "Electricity", 50.75, "Electricity Fee for 202601");
+
+        tracker.printAll();
+
+        System.out.println("Total spent: $" + tracker.totalSpent());
+        System.out.println("Total spent on Food: $" + tracker.totalForCategory("Food"));
+        System.out.println("Remove index 1");
+        tracker.remove(1);
+        tracker.printAll();
+        System.out.println("Total spent after remove");
+        System.out.println("Total spent(after remove): $" + tracker.totalSpent());
+        System.out.println("Expenses in 202602");
+        for (String s : tracker.expensesInMonth("202602")){
+            System.out.println(s);
+        }
+
+    }
 
 
 }
